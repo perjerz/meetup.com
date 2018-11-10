c6bed54f109fda494e15e2518375a2369e80e1b1
perjerz3434 <perjerz3434@gmail.com>
2018-11-11 02:29:25 +0700

    remove files

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { run } from '@cycle/run';
-import { getDrivers, wrapMain } from './drivers';
-import { Component } from './interfaces';
-import { App } from './components/app';
-
-const main: Component<any> = wrapMain(App);
-
-run(main as any, getDrivers());
